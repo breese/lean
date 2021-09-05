7cee1d92ca939d01ead3456d4fb37063e0840d3b
Bjorn Reese <breese@users.sourceforge.net>
2021-09-05 16:58:01 +0200

    Added assert_throw_with() macro

--- test/test_assert.hpp
+++ test/test_assert.hpp
@@ -16,5 +16,6 @@
 
 #define assert_throw(expression) do { try { expression; assert(false); } catch(...) {}; } while(false)
 #define assert_nothrow(expression) do { try { expression; } catch (...) { assert(false); } } while(false)
+#define assert_throw_with(expression,exception) do { try { expression; assert(false); } catch(const exception&) {}; } while(false)
 
 #endif // LEAN_TEST_ASSERT_HPP
