68804ed2c38fb39fd1150eafe5711499b10c03c3
Bjorn Reese <breese@users.sourceforge.net>
2021-10-02 17:09:16 +0200

    Added detail::function_type

---
+++ include/lean/detail/function_type.hpp
@@ -0,0 +1,270 @@
+#ifndef LEAN_DETAIL_FUNCTION_TYPE_HPP
+#define LEAN_DETAIL_FUNCTION_TYPE_HPP
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2021 Bjorn Reese <breese@users.sourceforge.net>
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace lean
+{
+namespace v1
+{
+namespace detail
+{
+
+//-----------------------------------------------------------------------------
+
+template <typename F, typename...>
+struct function_type
+    : public function_type<decltype(&F::operator())>
+{
+};
+
+//-----------------------------------------------------------------------------
+// R(Args...)
+
+template <typename R, typename... Args>
+struct function_type<R(Args...)>
+{
+    static constexpr bool is_const = false;
+    static constexpr bool is_noexcept = false;
+
+    using type = R(Args...);
+    using result_type = R;
+};
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) &>
+    : function_type<R(Args...)>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) &&>
+    : function_type<R(Args...)>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(*)(Args...)>
+    : function_type<R(Args...)>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...)>
+    : function_type<R(Args...)>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) &>
+    : function_type<R(Args...) &>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) &&>
+    : function_type<R(Args...) &&>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(&)(Args...)>
+    : function_type<R(Args...)>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(&&)(Args...)>
+    : function_type<R(Args...)>
+{
+};
+
+//-----------------------------------------------------------------------------
+// R(Args...) const
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) const>
+{
+    static constexpr bool is_const = true;
+    static constexpr bool is_noexcept = false;
+
+    using type = R(Args...) const;
+    using result_type = R;
+};
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) const &>
+    : function_type<R(Args...) const>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) const &&>
+    : function_type<R(Args...) const>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) const>
+    : function_type<R(Args...) const>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) const &>
+    : function_type<R(Args...) const &>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) const &&>
+    : function_type<R(Args...) const &&>
+{
+};
+
+//-----------------------------------------------------------------------------
+// R(Args...) noexcept
+
+#if __cpp_noexcept_function_type
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) noexcept>
+{
+    static constexpr bool is_const = false;
+    static constexpr bool is_noexcept = true;
+
+    using type = R(Args...) noexcept;
+    using result_type = R;
+};
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) & noexcept>
+    : function_type<R(Args...) noexcept>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) && noexcept>
+    : function_type<R(Args...) noexcept>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(*)(Args...) noexcept>
+    : function_type<R(Args...) noexcept>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(&)(Args...) noexcept>
+    : function_type<R(Args...) noexcept>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(&&)(Args...) noexcept>
+    : function_type<R(Args...) noexcept>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) noexcept>
+    : function_type<R(Args...) noexcept>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) & noexcept>
+    : function_type<R(Args...) noexcept>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) && noexcept>
+    : function_type<R(Args...) noexcept>
+{
+};
+
+#endif
+
+//-----------------------------------------------------------------------------
+// R(Args...) const noexcept
+
+#if __cpp_noexcept_function_type
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) const noexcept>
+{
+    static constexpr bool is_const = true;
+    static constexpr bool is_noexcept = true;
+
+    using type = R(Args...) const noexcept;
+    using result_type = R;
+};
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) const & noexcept>
+    : function_type<R(Args...) const noexcept>
+{
+};
+
+template <typename R, typename... Args>
+struct function_type<R(Args...) const && noexcept>
+    : function_type<R(Args...) const noexcept>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) const noexcept>
+    : function_type<R(Args...) const noexcept>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) const & noexcept>
+    : function_type<R(Args...) const noexcept>
+{
+};
+
+template <typename R, typename Class, typename... Args>
+struct function_type<R(Class::*)(Args...) const && noexcept>
+    : function_type<R(Args...) const noexcept>
+{
+};
+
+#endif
+
+//-----------------------------------------------------------------------------
+// Convenience
+
+template <typename F>
+using function_type_t = typename function_type<F>::type;
+
+template <typename F>
+using result_type_t = typename function_type<F>::result_type;
+
+template <typename F>
+struct is_noexcept
+    : public std::integral_constant<bool, function_type<F>::is_noexcept>
+{
+};
+
+template <typename F>
+struct is_const
+    : public std::integral_constant<bool, function_type<F>::is_const>
+{
+};
+
+} // namespace detail
+} // namespace v1
+} // namespace lean
+
+#endif // LEAN_DETAIL_FUNCTION_TYPE_HPP
---
+++ test/function_type_suite.cpp
@@ -0,0 +1,290 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2021 Bjorn Reese <breese@users.sourceforge.net>
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "test_assert.hpp"
+#include <type_traits>
+#include <lean/detail/function_type.hpp>
+
+//-----------------------------------------------------------------------------
+
+class my_class;
+
+//-----------------------------------------------------------------------------
+
+namespace type_suite
+{
+
+using lean::v1::detail::function_type_t;
+
+static_assert(std::is_same<function_type_t<void()>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void() &>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void() &&>, void()>::value, "");
+
+static_assert(std::is_same<function_type_t<void() const>, void() const>::value, "");
+static_assert(std::is_same<function_type_t<void() const &>, void() const>::value, "");
+static_assert(std::is_same<function_type_t<void() const &&>, void() const>::value, "");
+
+static_assert(std::is_same<function_type_t<void(*)()>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void(&)()>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void(&&)()>, void()>::value, "");
+
+static_assert(std::is_same<function_type_t<void(my_class::*)()>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() &>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() &&>, void()>::value, "");
+
+static_assert(std::is_same<function_type_t<void(my_class::*)() const>, void() const>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() const &>, void() const>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() const &&>, void() const>::value, "");
+
+#if __cpp_noexcept_function_type
+
+static_assert(std::is_same<function_type_t<void() noexcept>, void() noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void() & noexcept>, void() noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void() && noexcept>, void() noexcept>::value, "");
+
+static_assert(std::is_same<function_type_t<void() const noexcept>, void() const noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void() const & noexcept>, void() const noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void() const && noexcept>, void() const noexcept>::value, "");
+
+static_assert(std::is_same<function_type_t<void(*)() noexcept>, void() noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void(&)() noexcept>, void() noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void(&&)() noexcept>, void() noexcept>::value, "");
+
+static_assert(std::is_same<function_type_t<void(my_class::*)() noexcept>, void() noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() & noexcept>, void() noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() && noexcept>, void() noexcept>::value, "");
+
+static_assert(std::is_same<function_type_t<void(my_class::*)() const noexcept>, void() const noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() const & noexcept>, void() const noexcept>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() const && noexcept>, void() const noexcept>::value, "");
+
+
+#else
+
+static_assert(std::is_same<function_type_t<void() noexcept>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void() & noexcept>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void() && noexcept>, void()>::value, "");
+
+static_assert(std::is_same<function_type_t<void() const noexcept>, void() const>::value, "");
+static_assert(std::is_same<function_type_t<void() const & noexcept>, void() const>::value, "");
+static_assert(std::is_same<function_type_t<void() const && noexcept>, void() const>::value, "");
+
+static_assert(std::is_same<function_type_t<void(*)() noexcept>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void(&)() noexcept>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void(&&)() noexcept>, void()>::value, "");
+
+static_assert(std::is_same<function_type_t<void(my_class::*)() noexcept>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() & noexcept>, void()>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() && noexcept>, void()>::value, "");
+
+static_assert(std::is_same<function_type_t<void(my_class::*)() const noexcept>, void() const>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() const & noexcept>, void() const>::value, "");
+static_assert(std::is_same<function_type_t<void(my_class::*)() const && noexcept>, void() const>::value, "");
+
+#endif
+
+// Trailing return type
+
+static_assert(std::is_same<function_type_t<auto() -> void>, void()>::value, "");
+static_assert(std::is_same<function_type_t<auto(*)() -> void>, void()>::value, "");
+static_assert(std::is_same<function_type_t<auto(&)() -> void>, void()>::value, "");
+static_assert(std::is_same<function_type_t<auto(&&)() -> void>, void()>::value, "");
+static_assert(std::is_same<function_type_t<auto() const -> void>, void() const>::value, "");
+
+} // namespace type_suite
+
+//-----------------------------------------------------------------------------
+
+namespace result_type_suite
+{
+
+using lean::v1::detail::result_type_t;
+
+static_assert(std::is_same<result_type_t<void()>, void>::value, "");
+static_assert(std::is_same<result_type_t<void() &>, void>::value, "");
+static_assert(std::is_same<result_type_t<void() &&>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void() const>, void>::value, "");
+static_assert(std::is_same<result_type_t<void() const &>, void>::value, "");
+static_assert(std::is_same<result_type_t<void() const &&>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void(*)()>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(&)()>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(&&)()>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void(my_class::*)()>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(my_class::*)() &>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(my_class::*)() &&>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void(my_class::*)() const>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(my_class::*)() const &>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(my_class::*)() const &&>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void() noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void() & noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void() && noexcept>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void() const noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void() const & noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void() const && noexcept>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void(*)() noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(&)() noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(&&)() noexcept>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void(my_class::*)() noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(my_class::*)() & noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(my_class::*)() && noexcept>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void(my_class::*)() const noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(my_class::*)() const & noexcept>, void>::value, "");
+static_assert(std::is_same<result_type_t<void(my_class::*)() const && noexcept>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<auto() -> void>, void>::value, "");
+static_assert(std::is_same<result_type_t<auto(*)() -> void>, void>::value, "");
+static_assert(std::is_same<result_type_t<auto(&)() -> void>, void>::value, "");
+static_assert(std::is_same<result_type_t<auto(&&)() -> void>, void>::value, "");
+static_assert(std::is_same<result_type_t<auto() const -> void>, void>::value, "");
+
+static_assert(std::is_same<result_type_t<void *()>, void *>::value, "");
+static_assert(std::is_same<result_type_t<const void *()>, const void *>::value, "");
+
+} // namespace result_type_suite
+
+//-----------------------------------------------------------------------------
+
+namespace is_noexcept_suite
+{
+
+using lean::v1::detail::is_noexcept;
+
+static_assert(!is_noexcept<void()>::value, "");
+static_assert(!is_noexcept<void() &>::value, "");
+static_assert(!is_noexcept<void() &&>::value, "");
+
+static_assert(!is_noexcept<void() const>::value, "");
+static_assert(!is_noexcept<void() const &>::value, "");
+static_assert(!is_noexcept<void() const &&>::value, "");
+
+static_assert(!is_noexcept<void(*)()>::value, "");
+static_assert(!is_noexcept<void(&)()>::value, "");
+static_assert(!is_noexcept<void(&&)()>::value, "");
+
+static_assert(!is_noexcept<void(my_class::*)()>::value, "");
+static_assert(!is_noexcept<void(my_class::*)() &>::value, "");
+static_assert(!is_noexcept<void(my_class::*)() &&>::value, "");
+
+static_assert(!is_noexcept<void(my_class::*)() const>::value, "");
+static_assert(!is_noexcept<void(my_class::*)() const &>::value, "");
+static_assert(!is_noexcept<void(my_class::*)() const &&>::value, "");
+
+#if __cpp_noexcept_function_type
+
+static_assert(is_noexcept<void() noexcept>::value, "");
+static_assert(is_noexcept<void() & noexcept>::value, "");
+static_assert(is_noexcept<void() && noexcept>::value, "");
+
+static_assert(is_noexcept<void() const noexcept>::value, "");
+static_assert(is_noexcept<void() const & noexcept>::value, "");
+static_assert(is_noexcept<void() const && noexcept>::value, "");
+
+static_assert(is_noexcept<void(*)() noexcept>::value, "");
+static_assert(is_noexcept<void(&)() noexcept>::value, "");
+static_assert(is_noexcept<void(&&)() noexcept>::value, "");
+
+static_assert(is_noexcept<void(my_class::*)() noexcept>::value, "");
+static_assert(is_noexcept<void(my_class::*)() & noexcept>::value, "");
+static_assert(is_noexcept<void(my_class::*)() && noexcept>::value, "");
+
+static_assert(is_noexcept<void(my_class::*)() const noexcept>::value, "");
+static_assert(is_noexcept<void(my_class::*)() const & noexcept>::value, "");
+static_assert(is_noexcept<void(my_class::*)() const && noexcept>::value, "");
+
+#else
+
+static_assert(!is_noexcept<void() noexcept>::value, "");
+static_assert(!is_noexcept<void() & noexcept>::value, "");
+static_assert(!is_noexcept<void() && noexcept>::value, "");
+
+static_assert(!is_noexcept<void() const noexcept>::value, "");
+static_assert(!is_noexcept<void() const & noexcept>::value, "");
+static_assert(!is_noexcept<void() const && noexcept>::value, "");
+
+static_assert(!is_noexcept<void(*)() noexcept>::value, "");
+static_assert(!is_noexcept<void(&)() noexcept>::value, "");
+static_assert(!is_noexcept<void(&&)() noexcept>::value, "");
+
+static_assert(!is_noexcept<void(my_class::*)() noexcept>::value, "");
+static_assert(!is_noexcept<void(my_class::*)() & noexcept>::value, "");
+static_assert(!is_noexcept<void(my_class::*)() && noexcept>::value, "");
+
+static_assert(!is_noexcept<void(my_class::*)() const noexcept>::value, "");
+static_assert(!is_noexcept<void(my_class::*)() const & noexcept>::value, "");
+static_assert(!is_noexcept<void(my_class::*)() const && noexcept>::value, "");
+
+#endif
+
+} // namespace is_noexcept_suite
+
+//-----------------------------------------------------------------------------
+
+namespace is_const_suite
+{
+
+using lean::v1::detail::is_const;
+
+static_assert(!is_const<void()>::value, "");
+static_assert(!is_const<void() &>::value, "");
+static_assert(!is_const<void() &&>::value, "");
+
+static_assert(is_const<void() const>::value, "");
+static_assert(is_const<void() const &>::value, "");
+static_assert(is_const<void() const &&>::value, "");
+
+static_assert(!is_const<void(*)()>::value, "");
+static_assert(!is_const<void(&)()>::value, "");
+static_assert(!is_const<void(&&)()>::value, "");
+
+static_assert(!is_const<void(my_class::*)()>::value, "");
+static_assert(!is_const<void(my_class::*)() &>::value, "");
+static_assert(!is_const<void(my_class::*)() &&>::value, "");
+
+static_assert(is_const<void(my_class::*)() const>::value, "");
+static_assert(is_const<void(my_class::*)() const &>::value, "");
+static_assert(is_const<void(my_class::*)() const &&>::value, "");
+
+static_assert(!is_const<void() noexcept>::value, "");
+static_assert(!is_const<void() & noexcept>::value, "");
+static_assert(!is_const<void() && noexcept>::value, "");
+
+static_assert(is_const<void() const noexcept>::value, "");
+static_assert(is_const<void() const & noexcept>::value, "");
+static_assert(is_const<void() const && noexcept>::value, "");
+
+static_assert(!is_const<void(*)() noexcept>::value, "");
+static_assert(!is_const<void(&)() noexcept>::value, "");
+static_assert(!is_const<void(&&)() noexcept>::value, "");
+
+static_assert(!is_const<void(my_class::*)() noexcept>::value, "");
+static_assert(!is_const<void(my_class::*)() & noexcept>::value, "");
+static_assert(!is_const<void(my_class::*)() && noexcept>::value, "");
+
+static_assert(is_const<void(my_class::*)() const noexcept>::value, "");
+static_assert(is_const<void(my_class::*)() const & noexcept>::value, "");
+static_assert(is_const<void(my_class::*)() const && noexcept>::value, "");
+
+} // namespace is_const_suite
+
+//-----------------------------------------------------------------------------
+
+int main()
+{
+    return 0;
+}
